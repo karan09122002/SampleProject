be6bdd106278858a8175556af5401ad3cf2108cc
unknown <karanrijhwani108y@gmail.com>
2024-11-26 20:29:51 +0530

    changes made in Hello.java

--- Hello.java
+++ Hello.java
@@ -2,6 +2,6 @@ class Hello
 {
 	public static void main(String args[])
 	{
-		System.out.println("Hello Namste , NAMASKAARAM	");
+		System.out.println("Hello,Good Day, Namste , NAMASKAARAM	");
 	}
 }
